e0ff8cbaa7e4e2b5238f1e1641883a248588ad85
Codeweavers Infrastructure <36475663+infraweavers@users.noreply.github.com>
2021-09-20 13:20:38 +0100

    Standardising with check_script_via_monitoring_agent

--- check_monitoring-agent.pl
+++ check_monitoring-agent.pl
@@ -4,13 +4,14 @@ use JSON;
 use LWP::UserAgent;
 use Data::Dumper;
 use Monitoring::Plugin;
+use MIME::Base64;
 
 my $plugin = Monitoring::Plugin->new (
-        usage => '',
-        plugin => $0,
-        shortname => 'Check monitoring-agent info',
-        blurb => 'Checks monitoring-agent info',
-        timeout => 10
+	usage => '',
+	plugin => $0,
+	shortname => 'Check monitoring-agent info',
+	blurb => 'Checks monitoring-agent info',
+	timeout => "10s"
 );
 
 my $units = {
@@ -25,9 +26,10 @@ my $units = {
 
 my $user_agent = LWP::UserAgent->new();
 
-$plugin->add_arg(spec => 'insecure|i!', help => 'ignore TLS Certificate checks', required=> 0);
 $plugin->add_arg(spec => 'hostname|h=s', help => 'hostname or ip', required => 1);
 $plugin->add_arg(spec => 'port|p=i', help => 'port number', required => 1);
+$plugin->add_arg(spec => 'insecure|i!', help => 'ignore TLS Certificate checks', required=> 0);
+$plugin->add_arg(spec => 'cacert|e=s', help => 'CA certificate', required => 0);
 $plugin->add_arg(spec => 'certificate|c=s', help => 'certificate file', required => 0);
 $plugin->add_arg(spec => 'key|k=s', help => 'key file', required => 0);
 $plugin->add_arg(spec => 'username|u=s', help => 'username', required => 0);
@@ -38,27 +40,34 @@ $plugin->getopts;
 my $username = exists($ENV{'MONITORING_AGENT_USERNAME'}) ? $ENV{'MONITORING_AGENT_USERNAME'} : $plugin->opts->username;
 my $password = exists($ENV{'MONITORING_AGENT_PASSWORD'}) ? $ENV{'MONITORING_AGENT_PASSWORD'} : $plugin->opts->password;
 
-my $socket = $plugin->opts->hostname . ":" . $plugin->opts->port;
+my $base64creds = encode_base64("$username" . ":" . "$password");
+chomp($base64creds);
 
-$user_agent->credentials($socket, "Access restricted", $username, $password);
+my $socket = $plugin->opts->hostname . ":" . $plugin->opts->port;
 
 if($plugin->opts->insecure) {
 	$user_agent->ssl_opts(verify_hostname => 0, SSL_verify_mode => 0x00);	
 }
-
-if( -e $plugin->opts->certificate) {
+if($plugin->opts->cacert){
+	$user_agent->ssl_opts( "SSL_ca_file" => $plugin->opts->cacert );
+}
+if($plugin->opts->certificate) {
 	if( ! -e $plugin->opts->key ) {
 		print "invalid key file.\n";
 		exit UNKNOWN;
 	}
 
-	$user_agent->ssl_opts(SSL_cert_file => $plugin->opts->certificate, SSL_key_file => $plugin->opts->key);
+	$user_agent->ssl_opts(
+		SSL_cert_file => $plugin->opts->certificate, 
+		SSL_key_file => $plugin->opts->key
+	);
 }
 
 alarm $plugin->opts->timeout;
 
 my $response = $user_agent->get("https://$socket/v1/info",
-	'Content-Type'=> 'application/json'
+	'Content-Type'=> 'application/json',
+	'Authorization' => "Basic $base64creds",
 );
 
 if ($response->code ne 200) {
